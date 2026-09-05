22d9498df85a0ee2c99fa28cc199a630e9b47415
Pham Nguyen~ <emailcuaban@gmail.com>
2026-09-05 12:11:10 +0700

    Cập nhật dự án lesson 04

---
+++ Pdn_leson05/Pdn_leson05/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Pdn_leson05.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
