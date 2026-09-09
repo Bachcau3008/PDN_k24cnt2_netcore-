5c5a33005516f6a262e28c057e91b5456cef12ba
Pham Nguyen~ <emailcuaban@gmail.com>
2026-09-09 16:27:09 +0700

    Cập nhật dự án lesson 04

---
+++ PdnLesson06/PdnLesson06/ViewComponent/CategoryViewComponent.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PdnLesson06.Models;
+
+
+namespace PdnLesson06.ViewComponents 
+{
+    public class CategoryViewComponent : ViewComponent
+    {
+        public IViewComponentResult Invoke(int? n)
+        {
+            List<Category> categories = new List<Models.Category>
+            {
+                new Models.Category { CategoryId = 1, CategoryName = "Electronics", IsActive = true },
+                new Models.Category { CategoryId = 2, CategoryName = "Books", IsActive = true },
+                new Models.Category { CategoryId = 3, CategoryName = "Clothing", IsActive = false },
+                new Models.Category { CategoryId = 4, CategoryName = "Home & Kitchen", IsActive = true }
+            };
+
+            n = n ?? 0;
+            var search = categories.Where(c => c.CategoryId >n).ToList();
+            return View(search);
+        }
+    }
+}
